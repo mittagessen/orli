e7ae5f3182f1d336b0051dfca896aa38da2c2360
Benjamin Kiessling <mittagessen@l.unchti.me>
2024-10-07 23:48:25 +0200

    Revert "Pad to longest sequence length"
    This reverts commit eb63c76a79fa7c9e4a0cb2b3a971a120d93fe859.

--- transformer_seg/dataset.py
+++ transformer_seg/dataset.py
@@ -17,19 +17,19 @@ Utility functions for data loading and training of VGSL networks.
 """
 import io
 import torch
-import torch.nn.functional as F
 import lightning.pytorch as L
 
 import pyarrow as pa
 
-from functools import partial
-from typing import TYPE_CHECKING, Union, Sequence
+from typing import (TYPE_CHECKING, Callable, List, Literal, Optional, Union,
+                    Sequence)
 
 from torch.utils.data import Dataset, DataLoader
 
 from PIL import Image
 
 from torch.utils.data import default_collate
+from torch.nn.utils.rnn import pad_sequence
 
 from transformers import DonutImageProcessor
 
@@ -43,12 +43,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def collate_curves(batch, max_lines_in_page: int):
+def collate_curves(batch):
     """
     Concatenates and pads curves.
     """
     return {'image': default_collate([item['image'] for item in batch]),
-            'target': torch.stack([F.pad(x['target'], pad=(0, max_lines_in_page-len(x['target'])), value=-100) for x in batch])}
+            'target': pad_sequence([item['target'] for item in batch], batch_first=True, padding_value=-100)}
 
 
 def _validation_worker_init_fn(worker_id):
@@ -88,9 +88,6 @@ class LineSegmentationDataModule(L.LightningDataModule):
                                                    eos_token_id=self.hparams.eos_token_id,
                                                    curve_resolution=self.hparams.curve_resolution,
                                                    augmentation=False)
-        self.collator = partial(collate_curves,
-                                max_lines_in_page=max(self.train_set.max_lines_in_page,
-                                                      self.val_set.max_lines_in_page))
 
     def train_dataloader(self):
         return DataLoader(self.train_set,
@@ -98,7 +95,7 @@ class LineSegmentationDataModule(L.LightningDataModule):
                           num_workers=self.hparams.num_workers,
                           pin_memory=True,
                           shuffle=True,
-                          collate_fn=self.collator)
+                          collate_fn=collate_curves)
 
     def val_dataloader(self):
         return DataLoader(self.val_set,
@@ -107,7 +104,7 @@ class LineSegmentationDataModule(L.LightningDataModule):
                           num_workers=self.hparams.num_workers,
                           pin_memory=True,
                           worker_init_fn=_validation_worker_init_fn,
-                          collate_fn=self.collator)
+                          collate_fn=collate_curves)
 
 
 class BaselineSegmentationDataset(Dataset):
@@ -132,15 +129,13 @@ class BaselineSegmentationDataset(Dataset):
         self.arrow_table = None
         self.curve_resolution = curve_resolution
         self.eos_token_id = eos_token_id
-        self.max_lines_in_page = 0
 
         for file in files:
             with pa.memory_map(file, 'rb') as source:
                 ds_table = pa.ipc.open_file(source).read_all()
                 raw_metadata = ds_table.schema.metadata
-                if not raw_metadata or b'max_lines_in_page' not in raw_metadata:
+                if not raw_metadata or b'num_lines' not in raw_metadata:
                     raise ValueError(f'{file} does not contain a valid metadata record.')
-                self.max_lines_in_page = max(int.from_bytes(raw_metadata[b'max_lines_in_page'], 'little'), self.max_lines_in_page)
                 if not self.arrow_table:
                     self.arrow_table = ds_table
                 else:
