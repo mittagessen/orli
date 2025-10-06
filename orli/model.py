Fix loss normalization

orli/model.py
@@ -59,12 +59,15 @@ def model_step(model,
 
     logits = model(tokens=torch.cat([tokens, curves], dim=-1), encoder_input=batch['image'])
     losses = None
+    O, B = logits['curves'].shape[:2]
+    num_lines = target_tokens[target_tokens != -1].view(-1, 4).shape[0]
+
     for pred_curves, pred_tokens in zip(logits['curves'], logits['tokens']):
         pred_curves = pred_curves.view(-1)[target_curves != -1]
         pred_tokens = pred_tokens[target_tokens != -1].view(-1, 4)
         _loss = 2 * cls_criterion(pred_tokens, target_tokens[target_tokens != -1].view(-1, 4)) + 5 * curve_criterion(pred_curves, target_curves[target_curves != -1])
         losses = _loss if not losses else losses + _loss
-    return losses / len(logits['curves'])
+    return losses / (O * B * num_lines)
 
 
 class SegmentationModel(L.LightningModule):