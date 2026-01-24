0ce4e4d1b91bdfd139b9817389f880b508775fd4
Benjamin Kiessling <mittagessen@l.unchti.me>
2026-01-24 13:22:50 +0100

    grayscale augmentation

--- orli/augmentation.py
+++ orli/augmentation.py
@@ -25,51 +25,19 @@ from torchvision.transforms import v2
 logger = logging.getLogger(__name__)
 
 
-class BoundRandomResize(v2.RandomShortestSize):
-    """
-    Randomly proportionally resizes an input image between upper and lower
-    image dimension bounds.
-
-    Only accepts tuple-d bounds.
-    """
-    def __init__(self,
-                 min_size: Union[List[int], Tuple[int]],
-                 max_size: Union[List[int], Tuple[int]],
-                 *args,
-                 **kwargs):
-        super().__init__(*args, **kwargs, min_size=min_size)
-        self.min_size = list(min_size)
-        self.max_size = list(max_size)
-
-    def make_params(self, flat_inputs: List[Any]) -> Dict[str, Any]:
-        orig_height, orig_width = v2._utils.query_size(flat_inputs)
-
-        # recompute lower and upper ratios so resized image falls into bounds
-        min_r = max(self.min_size[0]/orig_height, self.min_size[1]/orig_width)
-        max_r = min(self.max_size[0]/orig_height, self.max_size[1]/orig_width)
-        
-        # do not randomly scale if dimensions would fall outside bounds
-        if max_r < min_r:
-            r = max_r
-        else:
-            r = torch.FloatTensor(1).uniform_(min_r, max_r).item()
-        new_width = int(orig_width * r)
-        new_height = int(orig_height * r)
-
-        return dict(size=(new_height, new_width))
-
-
 class DefaultAugmenter:
     def __init__(self):
         import cv2
-        cv2.setNumThreads(0)
+        cv2.setNumThreads(2)
         from albumentations import (Blur, Compose, MedianBlur, MotionBlur,
-                                    OneOf, PixelDropout, ToFloat, ColorJitter)
+                                    ToGray, OneOf, PixelDropout, ToFloat,
+                                    ColorJitter)
 
         self._transforms = Compose([
                                     ToFloat(),
                                     PixelDropout(p=0.2),
                                     ColorJitter(p=0.5),
+                                    ToGray(p=0.2),
                                     OneOf([
                                         MotionBlur(p=0.2),
                                         MedianBlur(blur_limit=3, p=0.1),
